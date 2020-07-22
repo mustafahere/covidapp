a9eab73a021e7c70d22d89890e5da286b59719b9
Mustafa Jawed <46250157+mustafahere@users.noreply.github.com>
2020-07-22 20:17:44 +0500

    UI progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TrackerComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
